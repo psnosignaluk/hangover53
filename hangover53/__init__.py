d1201c2d8d152554ab355c092dd4fae08fddf44c
Paul Stevens <psnosignaluk@gmail.com>
2024-07-02 16:54:29 +0100

    Initial commit

---
+++ hangover53/__init__.py
@@ -0,0 +1,8 @@
+from .__version__ import __description__, __title__, __version__
+
+try:
+    from ._main import main
+except ImportError:
+    def main() -> None:
+        import sys
+        sys.exit(1)
---
+++ hangover53/__version__.py
@@ -0,0 +1,3 @@
+__title__ = 'hangover53'
+__description__ = 'Hunt down and alert on orphaned DNS records'
+__version__ = '0.1.0'
